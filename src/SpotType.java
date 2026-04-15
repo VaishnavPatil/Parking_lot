f55616a4439ce33730960bf3302565d71cd6ba82
Vaishnav <vaishnavpatil1143@gmail.com>
2026-04-15 20:04:23 +0530

    Some changes

--- src/SpotType.java
+++ src/SpotType.java
@@ -1,9 +1,9 @@
 public enum SpotType
 {
-    COMPACT("Compact"),
-    REGULAR("Regular"),
-    LARGE("Large"),
-    DISABLED("Disabled");
+    COMPACT("Compact", 1.0),
+    REGULAR("Regular", 1.5),
+    LARGE("Large", 2.5),
+    DISABLED("Disabled", 1.0);
     private String displayName;
     private final double hourlyRate;
     SpotType(String displayName, double hourlyRate)
